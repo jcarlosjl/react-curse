e92a7d935ecfd0e95c3ac78a4f4ac1b647709b9d
jcarlosjl <isc.jcjl@me.com>
2019-01-27 17:41:09 -0600

    Create initial Search Bar component

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import FontAwesome from 'react-fontawesome';
+import './SearchBar.css';
+
+export default class SearchBar extends Component {
+    state = {}
+
+    render () {
+        return(
+            <div>SearchBar</div>
+        )
+    }
+}
