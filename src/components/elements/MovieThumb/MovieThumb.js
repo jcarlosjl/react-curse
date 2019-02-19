eaabebf2c700260da6cb9bcfcec2eb789f7f9a12
jcarlosjl <isc.jcjl@me.com>
2019-02-18 22:13:21 -0600

    Add MovieThumb skeleton

---
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MovieThumb.css'
+
+const movieThumb = (props) => {
+    return (
+        <div>
+            MovieThumb
+        </div>
+    )
+};
+
+export default movieThumb;
