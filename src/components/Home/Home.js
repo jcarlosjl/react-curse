3a397ce6dd536deffcc3bc0e502dadc7aabad0c7
jcarlosjl <isc.jcjl@me.com>
2019-02-20 23:03:42 -0600

    Add logic to fetch data from the API

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,10 +6,50 @@ import FourColGrid from '../elements/FourColGrid/FourColGrid'
 import MovieThumb from '../elements/MovieThumb/MovieThumb'
 import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn'
 import Spinner from  '../elements/Spinner/Spinner'
+import {API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from '../../config'
 
 export default class Home extends Component {
     state = {
+        movies: [],
+        heroImage: null,
+        loading: false,
+        currentPage: 0,
+        totalPages: 0,
+        searchTerm: '',
+    }
 
+    componentDidMount() {
+        this.setState({loading: true});
+        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&lang=es-ES&page=1`;
+        this.fetchItems(endPoint);
+    }
+
+    fetchItems = (endPoint) => {
+        fetch(endPoint)
+        .then(result => result.json())
+        .then(result => {
+            console.log(result);
+            this.setState({
+                movies: [...this.state.movies, ...result.results],
+                heroImage: this.state.heroImage || result.results[0],
+                loading: false,
+                currentPage: result.page,
+                totalPages: result.total_pages
+            });
+        });
+    }
+
+    loadMoreItems = () => {
+        let endPoint = '';
+        this.setState({ loading: true });
+
+        if (this.state.searchTerm === '') {
+            endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&lang=es-ES&page=${this.state.currentPage + 1}`
+        } else {
+            endPoint = `${API_URL}search/movie?api_key=${API_KEY}&lang=es-ES&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`
+        }
+
+        this.fetchItems(endPoint);
     }
 
     render() {
