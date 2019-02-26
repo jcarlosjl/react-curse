9cd2c150cd59f4ba73ac401048b7c215f3d5bb5a
Juan Carlos Juarez <isc.jcjl@me.com>
2019-02-25 21:31:38 -0600

    Add link to movie page

--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './MovieThumb.css'
+import { Link } from 'react-router-dom';
 
 const MovieThumb = (props) => {
     return (
         <div className="rmdb-moviethumb">
+            {props.clickable ?
+            <Link to={{ pathname: `/${props.movieId}`, movieName: `${props.movieName}` }}>
+                <img src={props.image} alt="movie thumb"/>
+            </Link>
+            :
             <img src={props.image} alt="movie thumb"/>
+            }
         </div>
     )
 };
