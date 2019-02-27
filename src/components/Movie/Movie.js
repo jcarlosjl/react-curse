470b5a5027490ac80d492cdd089cc5cc3373473e
Juan Carlos Juarez <isc.jcjl@me.com>
2019-02-27 08:20:11 -0600

    Create final struct and fetch data

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,14 +3,72 @@ import { API_KEY, API_URL } from '../../config';
 import FourColGrid from '../elements/FourColGrid/FourColGrid';
 import Spinner from '../elements/Spinner/Spinner';
 import './Movie.css';
+import Navigation from '../elements/Navigation/Navigation';
+import MovieInfo from '../elements/MovieInfo/MovieInfo';
+import MovieInfoBar from '../elements/MovieInfoBar/MovieInfoBar';
+import Actor from '../elements/Actor/Actor';
 
 class Movie extends Component {
-    state = {  }
+    state = { 
+        movie: null,
+        actors: null,
+        directors: [],
+        loading: false,
+    }
+
+    componentDidMount() {
+        this.setState({loading: true});
+        const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=es-ES`;
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+        .then(result => result.json())
+        .then(result => {
+            console.log(result);
+            if (result.status_code) {
+                this.setState({ loading: false });
+            } else {
+                this.setState({movie: result}, () => {
+                    const endpoint = `${API_URL}movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}&language=es-ES`;
+                    fetch(endpoint)
+                    .then(result => result.json())
+                    .then(result => {
+                        const directors = result.crew.filter((member) => member.job === 'Director');
+                        this.setState({
+                            loading: false,
+                            actors: result.cast,
+                            directors
+                        });
+                    });
+                });
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    }
+
     render() {
         return (
-            <div>
-                <FourColGrid></FourColGrid>
-                <Spinner />
+            <div className="rmdb-movie">
+            {this.state.movie ? 
+                <div>
+                    <Navigation movie={this.props.location.movieName} />
+                    <MovieInfo movie={this.state.movie} directors={this.state.directors} />
+                    <MovieInfoBar time={this.state.movie.runtime} budget={this.state.movie.budget} revenue={this.state.movie.revenue} />
+                </div>
+                : null}
+            {this.state.actors ? 
+                <div className="rmdb-movie-grid">
+                    <FourColGrid header="Actors">
+                    {this.state.actors.map((element, i) => {
+                        return <Actor key={i} actor={element} />
+                    })}
+                    </FourColGrid>
+                </div>
+                : null}
+                {this.state.loading ? <Spinner /> : null}
+                {!this.state.actors && !this.state.loading ? <h1>No Movie Found!!!</h1> : null}
             </div>
         );
     }
